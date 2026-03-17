b3a0cfbed92749d137360e15b5eedc13b328f0cf
WolfBoy55014 <97041213+WolfBoy55014@users.noreply.github.com>
2026-03-16 23:11:05 -0500

    signals work again

--- tasks/drivers/serial_driver.c
+++ tasks/drivers/serial_driver.c
@@ -4,15 +4,16 @@
 
 #include "include/serial_driver.h"
 
+#include "kernel_config.h"
 #include "scheduler.h"
 
 void kelp_serial_driver(uint32_t pid, uint32_t* signals, char* args) {
-    uint32_t i = 0;
+    uint32_t l = 0;
 
 
     while (1) {
 
-        if (i >= 100) {
+        if (l >= 100) {
             // check signals
             if (*signals & TASK_SIGTERM) {
                 return;
@@ -20,13 +21,20 @@ void kelp_serial_driver(uint32_t pid, uint32_t* signals, char* args) {
         }
 
         // send input
-        int c = getchar_timeout_us(0);
+        int c = 0;
+        int num_chars = 0;
+        char chars[CHANNEL_SIZE - 3];
 
-        // TODO: Use input_str
-        while (c != PICO_ERROR_TIMEOUT) {
-            kelp_text_send_input_char((char) c);
+        for (uint16_t i = 0; i < CHANNEL_SIZE - 3; i++) {
             c = getchar_timeout_us(0);
+            if (c == PICO_ERROR_TIMEOUT) {
+                break;
+            }
+            chars[i] = c;
+            num_chars++;
         }
+
+        kelp_text_send_input_string(chars, num_chars);
 
         // get output
         c = kelp_text_read_output_char();
@@ -36,7 +44,7 @@ void kelp_serial_driver(uint32_t pid, uint32_t* signals, char* args) {
             c = kelp_text_read_output_char();
         }
 
-        i++;
+        l++;
 
         sleep_ms(1);
     }
